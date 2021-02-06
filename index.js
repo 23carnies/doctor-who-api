f01d21a99ee1e7a0f32bed26adfbfc60fbdec026
23carnies <46233990+23carnies@users.noreply.github.com>
2021-02-06 16:29:59 -0600

    Add playground

--- index.js
+++ index.js
@@ -242,7 +242,7 @@ const resolvers = {
 }
 
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers, introspection: true, playground: true });
 
 server.listen({
     port: process.env.PORT || 4000
